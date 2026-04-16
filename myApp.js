a0635478b271b07c6065996b9033cf8bbb9615f0
ouestdrancy <freiburg.im.breisgau@proton.me>
2026-04-16 14:13:50 +0400

    html

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function(req, res) {
+app.get('/views/index.html', function(req, res) {
     res.send('Hello Express');
 });
 
