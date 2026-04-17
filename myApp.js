d5cc7a3e95fed742dc7c444043ee0b729113965f
ouestdrancy <freiburg.im.breisgau@proton.me>
2026-04-17 18:00:46 +0400

    cor

--- myApp.js
+++ myApp.js
@@ -9,12 +9,10 @@ app.get('/', function(req, res) {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
-    /*let message = "Hello json";*/
+    let message = "Hello json";
 
     if (process.env.MESSAGE_STYLE === "uppercase") {
-        message = "Hello json".toUpperCase();
-    } else {
-        message = "Hello json";
+        message = message.toUpperCase();
     } 
 
     res.json({message: message});
