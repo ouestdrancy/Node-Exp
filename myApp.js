c0d5d55621dafde40b85f634872fbef90d91b794
ouestdrancy <freiburg.im.breisgau@proton.me>
2026-04-17 17:28:18 +0400

    env

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
@@ -8,8 +9,14 @@ app.get('/', function(req, res) {
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/json', function(req, res) {
-    res.json({"message": "Hello json"})
-} )
+    let message = "Hello json";
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+    }
+
+    res.json({ message: message });
+});
 
 
 
